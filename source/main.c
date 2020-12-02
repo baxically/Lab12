16c207ce9440e763b4a7c4ab4507870b58f0b914
Ally Thach <allythach379@gmail.com>
2020-12-01 23:05:04 -0800

    header + pt1

--- source/main.c
+++ source/main.c
@@ -10,14 +10,50 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include "timer.h"
 #endif
+
+unsigned char pattern;
+unsigned char row;
+unsigned char input;
+enum Row_States { Row_Init, Row_Shift } Row_currState;
+
+void RowShift() {
+    switch (Row_currState) {
+        case Row_Init:
+            input = ~PINA;
+            pattern = 0xFF;
+            row = 0x01;
+            Row_currState = Row_Shift;
+            break;
+        case Row_Shift:
+            input = ~PINA;
+            if (input == 0x02 && row < 0x10) {
+                row <<= 1;
+                Row_currState = Row_Shift;
+            } else if (input == 0x01 && row > 0x01) {
+                row >>= 1;
+                Row_currState = Row_Shift;
+            } else { Row_currState = Row_Shift; }
+            break;
+    }
+    PORTC = pattern; PORTD = ~row;
+}
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
 
+    TimerSet(100);
+    Row_currState = Row_Init;
+    TimerOn();
     /* Insert your solution below */
     while (1) {
-
+        RowShift();
+        while(!TimerFlag);
+        TimerFlag = 0;
     }
     return 1;
 }
