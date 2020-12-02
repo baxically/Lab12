ac6b04bde30cf422a00611db43622862a22f9ddf
Ally Thach <allythach379@gmail.com>
2020-12-01 23:13:17 -0800

    part1 test

--- source/main.c
+++ source/main.c
@@ -13,47 +13,31 @@
 #include "timer.h"
 #endif
 
-unsigned char patternArray[2] = { 0x3C, 0x24 };
-unsigned char rowArray[3] = { 0x02, 0x04, 0x08 };
+unsigned char pattern;
+unsigned char row;
 unsigned char input;
-unsigned char i;
+enum Row_States { Row_Init, Row_Shift } Row_currState;
 
-enum CenterRect_States { CR_Init, CR_DisplayR2, CR_DisplayR3, CR_DisplayR4 } CR_currState;
-
-void CenterRectDisplay() {
-    switch (CR_currState) {
-        case CR_Init:
-            CR_currState = CR_DisplayR2;
+void RowShift() {
+    switch (Row_currState) {
+        case Row_Init:
+            input = ~PINA;
+            pattern = 0xFF;
+            row = 0x01;
+            Row_currState = Row_Shift;
             break;
-        case CR_DisplayR2:
-            CR_currState = CR_DisplayR3;
-            break;
-        case CR_DisplayR3:
-            CR_currState = CR_DisplayR4;
-            break;
-        case CR_DisplayR4:
-            CR_currState = CR_DisplayR2;
-            break;
-        default:
+        case Row_Shift:
+            input = ~PINA;
+            if (input == 0x02 && row < 0x10) {
+                row <<= 1;
+                Row_currState = Row_Shift;
+            } else if (input == 0x01 && row > 0x01) {
+                row >>= 1;
+                Row_currState = Row_Shift;
+            } else { Row_currState = Row_Shift; }
             break;
     }
-    
-    switch (CR_currState) {
-        case CR_DisplayR2:
-            PORTC = patternArray[0];
-            PORTD = ~rowArray[0];
-            break;
-        case CR_DisplayR3:
-            PORTC = patternArray[1];
-            PORTD = ~rowArray[1];
-            break;
-        case CR_DisplayR4:
-            PORTC = patternArray[0];
-            PORTD = ~rowArray[2];
-            break;
-        default:
-            break;
-    }
+    PORTC = pattern; PORTD = ~row;
 }
 
 int main(void) {
@@ -62,12 +46,12 @@ int main(void) {
     DDRC = 0xFF; PORTC = 0x00;
     DDRD = 0xFF; PORTD = 0x00;
 
-    TimerSet(1);
-    CR_currState = CR_Init;
+    TimerSet(100);
+    Row_currState = Row_Init;
     TimerOn();
     /* Insert your solution below */
     while (1) {
-        CenterRectDisplay();
+        RowShift();
         while(!TimerFlag);
         TimerFlag = 0;
     }
